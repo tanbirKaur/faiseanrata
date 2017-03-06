d0c2af974f2cccfb5b5f1c92844a6971d5c488cc
tanbirKaur <tanbirkaur16@gmail.com>
2017-03-07 01:19:50 +0530

    Show all designs of collection in collections page.

--- app/controllers/collectionController.js
+++ app/controllers/collectionController.js
@@ -56,37 +56,8 @@ app.controller('collectionController', ['$stateParams', 'toastr', '$rootScope', 
         };
 
 
-        $(".fancybox").fancybox({
-            beforeShow: function () {
-                if (this.title) {
-                    // New line
-                    this.title += '<br />';
 
-                    // Add tweet button
-                    // console.log("Get index");
-                    var index = this.element[0].attributes[0].value;
-                    // console.log(index);
-                    $rootScope.new = "Hello";
-                    this.title += '';
 
-                    // Add FaceBook like button
-                    this.title += '<iframe src="//www.facebook.com/plugins/like.php?href=' + this.href + '&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:110px; height:23px;" allowTransparency="true"></iframe>';
-                }
-            },
-            afterShow: function() {
-                // Render tweet button
-                twttr.widgets.load();
-            },
-            helpers : {
-                title : {
-                    type: 'inside'
-                }
-            }
-        });
-
-        $rootScope.test1 = function(){
-            // console.log("Hello");
-        };
 
 
     }]);
